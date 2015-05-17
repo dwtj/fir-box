705a5a6f07244a2f19d20d161793b5d994e31422
David Johnston <dwtj@iastate.edu>
2015-05-17 16:20:49 -0500

    Scripts use full paths for `brew` and `stow`.
    I'm not exactly sure why this is necessary, but it was a workaround for
    a problem that came up while debugging these scripts.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,18 +1,20 @@
 #!/bin/bash -
 set -e
 
+BREW_CMD=/usr/local/bin/brew
+
 # Download and install Command Line Tools, if needed.
 if [[ ! -x /usr/bin/gcc ]]; then
     xcode-select --install
 fi
 
 # Download and install Homebrew, if needed.
-if [[ ! -x /usr/local/bin/brew ]]; then
+if [[ ! -x "$BREW_CMD" ]]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # Download and install Ansible, if needed.
 if [[ ! -x /usr/local/bin/ansible ]]; then
-    brew update
-    brew install ansible
+    "$BREW_CMD" update
+    "$BREW_CMD" install ansible
 fi
--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -
+set -e
 
 DOTFILES_HOME="$HOME/.dotfiles"
-DOTFILES_PROJECTS="git interpreters terminal vim"
+DOTFILES_PROJECTS="git interpreters osx terminal vim"
+
+STOW_CMD="/usr/local/bin/stow"
 
 if [[ ! -x "$DOTFILES_HOME" ]]; then
     git clone https://github.com/dwtj/dotfiles.git "$DOTFILES_HOME"
     cd "$DOTFILES_HOME"
     git submodule init
     git submodule update
-    stow -R -t ~ $DOTFILES_PROJECTS
+    $STOW_CMD -R -t ~ $DOTFILES_PROJECTS
+    #source ~/.osx
 fi
